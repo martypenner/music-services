0d73930a8e74a728c42866a56e9ac0297442adc5
Marty Penner <martypenner@users.noreply.github.com>
2022-11-21 21:17:36 -0500

    refactor: get rid of custom tailscale and use curl instead

--- navidrome/start.sh
+++ navidrome/start.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-/app/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
-/app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=navidrome
+tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
+tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=navidrome
 
 exec /app/navidrome
